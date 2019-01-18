acfe9b91862edde29533599eeea12b8b344bf9bd
jeremyscatigna <scatignaj@me.com>
2019-01-18 19:17:30 +0100

    fix config

--- config/website.js
+++ config/website.js
@@ -3,14 +3,14 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteTitleShort: 'Cara', // short_name for manifest
-  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-  siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Geekpreneur - Frenchy Front-end Engineer', // Navigation and Site Title
+  siteTitleAlt: 'Geekpreneur', // Alternative Site title for SEO
+  siteTitleShort: 'Geekpreneur', // short_name for manifest
+  siteHeadline: 'Frenchy Front-end Engineer', // Headline for schema.org JSONLD
+  siteUrl: 'https://geekpreneur.tech', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
+  siteDescription: 'Frenchy Front-end Engineer',
   author: 'LekoArts', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
