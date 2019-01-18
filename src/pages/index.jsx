1865061924192d090cde259e8f60f60768e1151a
jeremyscatigna <scatignaj@me.com>
2019-01-18 19:12:32 +0100

    update name and desc

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -71,28 +71,28 @@ const Index = () => (
         <Title>Projects</Title>
         <ProjectsWrapper>
           <ProjectCard
-            title="Freiheit"
+            title="Outer"
             link="https://www.behance.net/gallery/58937147/Freiheit"
             bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
           >
             This project is my entry to Adobe's #ChallengeYourPerspective contest.
           </ProjectCard>
           <ProjectCard
-            title="Harry Potter"
+            title="Hostee"
             link="https://www.behance.net/gallery/52915793/Harry-Potter"
             bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
           >
             I entered the DOCMA 2017 award with this Harry Potter inspired image.
           </ProjectCard>
           <ProjectCard
-            title="Tomb Raider"
+            title="Bros"
             link="https://www.behance.net/gallery/43907099/Tomb-Raider"
             bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
           >
             Recreation of a Tomb Raider Wallpaper (Fan Art)
           </ProjectCard>
           <ProjectCard
-            title="Eagle"
+            title="Kosmos"
             link="https://www.behance.net/gallery/38068151/Eagle"
             bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
           >
@@ -105,8 +105,8 @@ const Index = () => (
         <AboutHero>
           <Avatar src={avatar} alt="Jeremy Scatigna" />
           <AboutSub>
-            Frenchy Front-end Engineer slightly geek on the edges and optionally a Tech lover. I'm pretty good at
-            having fun and drinking beers and I developed some sniper skills at playing ping pong.
+            Frenchy Front-end Engineer slightly geek on the edges and optionally a Tech lover. I'm pretty good at having
+            fun and drinking beers and I developed some sniper skills at playing ping pong.
           </AboutSub>
         </AboutHero>
         <AboutDesc>
